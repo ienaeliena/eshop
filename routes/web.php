6970d27bdc87e9c518304bbe46882137c47d3bb0
MSI\iena_ <siti.fazlina@inx.my>
2022-03-25 11:27:57 +0800

    fix: update route destroy to delete

--- routes/web.php
+++ routes/web.php
@@ -73,14 +73,14 @@ Route::middleware(['auth', 'isAdmin'])->group(function () {
     Route::Post('insert-category',[CategoryController::class,'insert']);
     Route::get('edit-category/{id}', [CategoryController::class,'edit']);
     Route::put('update-category/{id}',[CategoryController::class,'update']);
-    Route::get('delete-category/{id}',[CategoryController::class,'destroy']);
+    Route::get('delete-category/{id}',[CategoryController::class,'delete']);
 
     Route::get('products',[ProductController::class,'index']);
     Route::get('add-products',[ProductController::class,'add']);
     Route::post('insert-product',[ProductController::class,'insert']);
     Route::get('edit-product/{id}',[ProductController::class,'edit']);
     Route::put('update-product/{id}',[ProductController::class,'update']);
-    Route::get('delete-product/{id}',[ProductController::class,'destroy']);
+    Route::get('delete-product/{id}',[ProductController::class,'delete']);
 
     Route::get('users',[FrontendController::class,'users']);
 
